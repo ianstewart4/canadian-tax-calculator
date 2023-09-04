0dcb8ec7328be89e91518b50798c120bd26e46fb
Ian Stewart <ianstewart4@outlook.com>
2023-09-04 15:02:41 -0500

    updated for react-router functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar'
+import { Route, Routes } from "react-router-dom";
+import { Dashboard } from "./pages/Dashboard";
+import { Calculator } from "./pages/Calculator";
 
 function App() {
-  const [count, setCount] = useState(0)
-  
-
-  
   return (
-  
     <>
-      <Navbar/>
-    <div className="bg-blue-500 dark:bg-gray-700 h-screen flex items-center justify-center">
-      <div>
-       
-      
-      <h1>This will be a practice Application</h1>
-      <div className="card">
-        <button className='bg-gray-200 dark:bg-blue-500 border-gray-700 rounded-md p-4' onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      </div>
-    </div>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/calculator" element={<Calculator />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
