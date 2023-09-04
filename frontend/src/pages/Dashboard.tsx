b27e4842aecc421aca8ae5e9154271082ed6f2b9
Ian Stewart <ianstewart4@outlook.com>
2023-09-04 15:03:47 -0500

    moved dashboard to it's own page

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,27 @@
+import Navbar from "../components/Navbar";
+import { useState } from "react";
+
+export const Dashboard = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <Navbar />
+      <div className="bg-blue-500 dark:bg-gray-700 h-screen flex items-center justify-center">
+        <div>
+          <h1>This will be a practice Application</h1>
+          <div className="card">
+            <button
+              className="bg-gray-200 dark:bg-blue-500 border-gray-700 rounded-md p-4"
+              onClick={() => setCount((count) => count + 1)}
+            >
+              count is {count}
+            </button>
+          </div>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
