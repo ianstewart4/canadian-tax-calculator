11c440cdbb6d0387205063f643f46b449d1b2a6e
Ian Stewart <ianstewart4@outlook.com>
2023-09-09 20:52:17 -0500

    started comparison page

--- frontend/src/pages/CompareProvincesPage.tsx
+++ frontend/src/pages/CompareProvincesPage.tsx
@@ -1,9 +1,19 @@
 import Navbar from "../components/Navbar";
+import ProvinceDropdown from "../components/ProvinceDropdown";
+import { useState } from "react";
 
 export const CompareProvincesPage = () => {
+  const [province, setProvince] = useState("");
+
+  const onChangeProvinceSelect = (e: FormEvent) => {
+    setProvince((e.target as HTMLOptionElement).value);
+  };
+
   return (
     <div>
       <Navbar />
+      <ProvinceDropdown onChangeProvinceSelect={onChangeProvinceSelect} />
+      <h1>{province}</h1>
     </div>
   );
 };
