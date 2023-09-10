90ee8e20db0a3b3160ca49066fae6651a769d2fc
Ian Stewart <ianstewart4@outlook.com>
2023-09-09 20:49:28 -0500

    Added provinceSelect functionality

--- frontend/src/pages/CalculatorPage.tsx
+++ frontend/src/pages/CalculatorPage.tsx
@@ -1,15 +1,23 @@
+import { FormEvent, useState } from "react";
 import DataCard from "../components/DataCard";
 import InputCard from "../components/InputCard";
 import Navbar from "../components/Navbar";
 import ProvinceDropdown from "../components/ProvinceDropdown";
 
 export const CalculatorPage = () => {
+  const [province, setProvince] = useState("");
+
+  const onChangeProvinceSelect = (e: FormEvent) => {
+    setProvince((e.target as HTMLOptionElement).value);
+  };
+
   return (
     <>
       <Navbar />
       <div className="flex justify-center mt-10">
         <div>
-          <ProvinceDropdown />
+          <ProvinceDropdown onChangeProvinceSelect={onChangeProvinceSelect} />
+          <h1 className="flex justify-center mt-10">{province}</h1>
           <div className="flex justify-center mt-10">
             <InputCard />
             <DataCard />
